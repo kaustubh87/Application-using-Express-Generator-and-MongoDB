ddf9cddc994fdd2e68edcc8bc8fb11bf00a426ef
kvin1087 <kaustubh87@gmail.com>
2017-06-26 20:12:13 -0500

    Added local-signup Strategy

--- server/config/passport.js
+++ server/config/passport.js
@@ -55,4 +55,47 @@ module.exports = function (passport) {
             }
         });
     }));
+
+        // Signup local strategy
+    passport.use('local-signup', new LocalStrategy({
+        // change default username and password, to email and password
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true
+    },
+    function(req, email, password, done) {
+        if (email)
+        // format to lower-case
+        email = email.toLowerCase();
+        // asynchronous
+        process.nextTick(function() {
+            // if the user is not already logged in:
+            if (!req.user) {
+                User.findOne({ 'local.email' :  email }, function(err, user) {
+                    // if errors
+                    if (err)
+                    return done(err);
+                    // check email
+                    if (user) {
+                        return done(null, false, req.flash('signupMessage', 'Wohh! the email is already taken.'));
+                    } else {
+                        // create the user
+                        var newUser = new User();
+                        // Get user name from req.body
+                        newUser.local.name = req.body.name;
+                        newUser.local.email = email;
+                        newUser.local.password = newUser.generateHash(password);
+                        // save data
+                        newUser.save(function(err) {
+                            if (err)
+                            throw err;
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            } else {
+                return done(null, req.user);
+            }
+        });
+    }));
 };
