cb0041635296f8050e17c2e5bf2048d0e41bfe1b
kvin1087 <kaustubh87@gmail.com>
2017-06-27 15:26:43 -0500

    Changes to the passport file

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,62 +1,49 @@
-var passport = require('passport')
-    , LocalStrategy = require('passport-local').Strategy;
-
+// load passport module
+var LocalStrategy    = require('passport-local').Strategy;
+// load up the user model
 var User = require('../models/users');
 
-module.exports = function (passport) {
-
-    passport.serializeUser(function (user, done) {
+module.exports = function(passport) {
+    // passport init setup
+    // serialize the user for the session
+    passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
-
-    passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
+    // deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
             done(err, user);
         });
     });
-    
-    //using local strategy
+    // using local strategy
     passport.use('local-login', new LocalStrategy({
-        //change default username and password to email and password
-        usernameField: 'email',
-        passwordField: 'password',
-        passReqToCallback: true
-
-    }, function (req, email, password, done) {
+        // change default username and password, to email and password
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true
+    },
+    function(req, email, password, done) {
         if (email)
-            email = email.toLowerCase();
-        process.nextTick(function () {
-
-            //if user is not logged in
-            if (!req.user) {
-                User.findOne({ 'local.email': email }, function (err, user) {
-
-                    if (err)
-                        return done(err);
-
-                    if (user) {
-                        return done(null, false, req.flash('signupMessage', 'Email already taken'));
-                    }
-                    else {
-                        var newUser = new User();
-                        newUser.local.name = req.body.name;
-                        newUser.local.email = email;
-                        newUser.local.password = newUser.generateHash(password);
-                        newUser.save(function (err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-
-                });
-            } else {
-                return done(null, req.user);
-            }
+        // format to lower-case
+        email = email.toLowerCase();
+        // process asynchronous
+        process.nextTick(function() {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                // if errors
+                if (err)
+                return done(err);
+                // check errors and bring the messages
+                if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.'));
+                if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Wohh! Wrong password.'));
+                // everything ok, get user
+                else
+                return done(null, user);
+            });
         });
     }));
-
-        // Signup local strategy
+    // Signup local strategy
     passport.use('local-signup', new LocalStrategy({
         // change default username and password, to email and password
         usernameField : 'email',
