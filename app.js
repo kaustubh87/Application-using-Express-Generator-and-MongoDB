6354e2e8d720c4b2a7782bab32a0f46eff2b14db
kvin1087 <kaustubh87@gmail.com>
2017-06-28 09:41:37 -0500

    Installed gravatar

--- app.js
+++ app.js
@@ -4,51 +4,38 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
-
-var index = require('./server/routes/index');
+// Import Routes
+var routes = require('./server/routes/index');
 var users = require('./server/routes/users');
-
-//Import comments controller
-
+// Import comments controller
 var comments = require('./server/controllers/comments');
 
-// ODM with mongoose
-
+// ODM With Mongoose
 var mongoose = require('mongoose');
-
 // Modules to store session
-
-var session = require('express-session');
+var session    = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-
-// Passport and Warning Flash modules
-
+// Import Passport and Warning flash modules
 var passport = require('passport');
 var flash = require('connect-flash');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, './server/views/pages'));
+app.set('views', path.join(__dirname, 'server/views/pages'));
 app.set('view engine', 'ejs');
 
-//Database configuration
-
-var config = require('./server/config/config');
-
-//connect to database
-
+// Database configuration
+var config = require('./server/config/config.js');
+// connect to our database
 mongoose.connect(config.url);
-
-mongoose.connection.on('error', function(){
-  console.error('MongoDB connection error. Check if mongodb is running');
+// Check if MongoDB is running
+mongoose.connection.on('error', function() {
+	console.error('MongoDB Connection Error. Make sure MongoDB is running.');
 });
 
-//Require passport configuration
-
+// Passport configuration
 require('./server/config/passport')(passport);
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -56,12 +43,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+app.use(require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    sourceMap: true
 }));
+// Setup public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // required for passport
@@ -77,20 +65,62 @@ app.use(session({
     })
 }));
 
+// Init passport authentication
 app.use(passport.initialize());
+// persistent login sessions
 app.use(passport.session());
+// flash messages
 app.use(flash());
 
-app.use('/', index);
-//app.use('/users', users);
-//Setting up comments route
-
-app.get('/comments', comments.hasAuthorization, comments.list );
+app.use('/', routes);
+app.use('/users', users);
+// Setup routes for comments
+app.get('/comments', comments.hasAuthorization, comments.list);
 app.post('/comments', comments.hasAuthorization, comments.create);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 
 module.exports = app;
 
 app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + server.address().port);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
 });
